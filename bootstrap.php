89c0ef6fa92361ff7ccb027ee392f2cce447f023
Sumithran <sumithran@outlook.in>
2019-08-27 12:17:39 +0530

    fixed typo

--- bootstrap.php
+++ bootstrap.php
@@ -6,5 +6,5 @@ require_once __DIR__.'/vendor/autoload.php';
 // Specify our Twig templates location
 $loader = new Twig_Loader_Filesystem(__DIR__.'/templates');
 
-// Instantiate our Twig
+ // Instantiate our Twig
 $twig = new Twig_Environment($loader);
